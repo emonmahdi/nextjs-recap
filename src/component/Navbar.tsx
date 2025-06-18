b727da7a69ea9796c8e08d47acce170109a8bb32
emonmahdi <developeremon1997@gmail.com>
2025-06-18 07:49:41 +0600

    Active link menu

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,29 +1,45 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const navItems = [
-    {title: 'Home', href:'/'},
-    {title: 'About', href:'/about'},
-    {title: 'Blog', href:'/blog'},
-    {title: 'Contact', href:'/contact'},
-]
+  { title: "Home", href: "/" },
+  { title: "About", href: "/about" },
+  { title: "Blog", href: "/blogs" },
+  { title: "Contact", href: "/contact" },
+];
 
 const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
-    <div className='flex justify-between max-w-7xl mx-auto'>
-        <div> 
-            <Link href='/'>Logo</Link>
-        </div>
-        <div>
-            <ul>
-                {
-                    navItems.map((item) => <Link key={item.href} href={item.href}>{item.title}</Link>)
-                }
-            </ul>
-        </div>
+    <div className="flex justify-between max-w-7xl mx-auto border-b py-3 bg-amber-200">
+      <div>
+        <Link href="/">Logo</Link>
+      </div>
+      <div>
+        <ul>
+          {navItems.map((item) => {
+            const isActive = pathname === item.href;
+            return (
+              <li key={item.href} className="flex px-3">
+                <Link
+                  href={item.href}
+                  className={`px-3 ${
+                    isActive ? "font-semibold text-orange-500" : "text-black"
+                  }`}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
